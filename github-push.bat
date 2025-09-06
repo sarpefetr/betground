0703362c1347b2220bd4938dff48c34a85cf2eaf
sarpefetr <sarpefetr@gmail.com>
2025-09-06 16:32:14 +0400

    supernovabet
    pp1

---
+++ github-push.bat
@@ -0,0 +1,7 @@
+@echo off
+echo GitHub'a push ediliyor...
+git remote add origin https://github.com/KULLANICI_ADINIZ/supernovabet.git
+git branch -M main
+git push -u origin main
+echo Tamamlandi!
+pause
